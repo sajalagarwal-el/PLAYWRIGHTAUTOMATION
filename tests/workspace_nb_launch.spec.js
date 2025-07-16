9e17e4f524b642354db9f4f67c4c193729f89726
sajalagarwal-el <sajal.agarwal@elucidata.io>
2025-07-16 14:56:14 +0530

    changes to launch NB

--- tests/workspace_nb_launch.spec.js
+++ tests/workspace_nb_launch.spec.js
@@ -1,23 +1,26 @@
 //const {test, expect} =require('@playwright/test')
 import { test, expect } from '@playwright/test';
 import { login } from './helpers';
+import { timeout } from '../playwright.config';
 
 console.log('Running NB Launch test');
-test('should perform move in diff location and fail at same location', async ({ page }) => {
+test('NB Launch', async ({ page }) => {
   //await login(page, "srishti.mahale@elucidata.io", "Polly@1234");
   await login(page);
 
   await page.goto("https://polly.elucidata.io/manage/workspaces/dashboard")
-  console.log('Navigatied to Workspaces dashboard');
+  console.log('Navigated to Workspaces dashboard');
   //going to a WS
   await page.locator("//h3[normalize-space()='test_8junew']").first().click();
   await page.locator("//span[normalize-space(text())='New Analysis']").click();
+  await page.waitForLoadState('networkidle');
   await page.locator("//p[normalize-space(text())='Polly Notebook']").click();
+  await page.waitForLoadState('networkidle');
 
 //selecting elucidata R&D from teh list
 // 1. Click the dropdown caret to open the suggestions
-await page.locator("button.p-autocomplete-dropdown").click();
 
+await page.locator("span[class='polly-icon caret-down ng-star-inserted']").click();
 
 // Step 1: Type the initial text to trigger dropdown suggestions
 await page.locator('input[placeholder="Search Client Organization"]').fill('Elucidata');
@@ -26,22 +29,27 @@ console.log('Filled "Elucidata" in the search input');
 await page.waitForSelector("li.p-autocomplete-item", { state: "visible", timeout: 120000 });
 console.log('Dropdown options loaded successfully');
 // Step 3: Click on the desired option by its label
-await page.locator("//li[contains(@class, 'p-autocomplete-item') and .//p[text()='ElucidataInc']]").click();
+await page.locator("//p[normalize-space()='ElucidataInc']").click();
 console.log('Selected "ElucidataInc" from the dropdown');
+//await page.waitForTimeout(10000);
+await page.locator("//input[@placeholder='Enter a Notebook name']").click();
 
 
 // Step 1: Open dropdown
+// Wait for the Docker dropdown to be visible before clicking
+//await page.waitForSelector("//span[@aria-label='Select a Docker']", { state: 'visible', timeout: 120000 });
 await page.locator("//span[@aria-label='Select a Docker']").click();
 
 
 // Optional: wait for the dropdown options to appear
 await page.waitForSelector("li.p-dropdown-item");
+await page.waitForTimeout(2000);
 console.log('Selected Docker from dropdown');
 // Step 2: Select the desired option
 await page.locator("li.p-dropdown-item", { hasText: 'Python 3.10: Notebook environment for Python 3.10 ' }).click();
 console.log('Selected Notebook environment from the dropdown');
 
-///////////////
+
 // 1. Click the dropdown trigger to open the options
 await page.locator("//span[@aria-label='Select a Machine']").click();
 
@@ -52,8 +60,6 @@ await page.waitForSelector("li.p-dropdown-item");
 await page.locator("li.p-dropdown-item", { hasText: 'PollyN medium:2 vCPU, 4GB RAM'}).click();
 console.log('Selected machine from the dropdown');
 
-/////////////////
-
 
 await page.locator("//div[contains(@class, 'button-container')]//span[normalize-space(text())='Launch']").click();
 console.log('Clicked on Launch button');
@@ -63,12 +69,8 @@ const newTab = await page.context().waitForEvent('page');
 await newTab.waitForLoadState();
 console.log('New tab opened for Polly Notebook', newTab.url());
 
-await page.locator("//p[normalize-space()='Your Notebook is launching']").waitFor({ state: 'visible', timeout: 10000 });
+await expect(newTab.locator("text=Your Notebook is launching")).toBeVisible({ timeout: 20000 });
 console.log('Notebook is launching, waiting for it to be ready');
-
- // expect("re-report.html").toContain('re-report.html')
-  //expect(true).toBeTruthy()
-
 
   await page.close();
 
